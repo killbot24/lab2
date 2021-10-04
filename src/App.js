767018c6706c690a0a5bf267422661bca1021868
thomas burke <piecraft24@gmail.com>
2021-10-04 19:58:13 +0100

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import { Component } from 'react';
 import { Header} from './componets/header'
 import { Footer} from './componets/footer'
+import { Content} from './componets/content'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,Nav,Container}from 'react-bootstrap';
 class App extends Component {
   render (){
     return(
     <div className="App">
+
+<Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
       <Header></Header>
-        <h1>hello world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}</h2>
+        <Content></Content>
         <Footer></Footer>
     </div>
   );
