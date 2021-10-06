090d52ff6ebc84167bad99f71489e3f7ad3afaf6
thomas burke <piecraft24@gmail.com>
2021-10-06 19:07:50 +0100

    Edited app.js

--- src/App.js
+++ src/App.js
@@ -6,26 +6,33 @@ import { Footer} from './componets/footer'
 import { Content} from './componets/content'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar,Nav,Container}from 'react-bootstrap';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 class App extends Component {
   render (){
     return(
+      <Router>
     <div className="App">
 
 <Navbar bg="primary" variant="dark">
     <Container>
     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/read">read</Nav.Link>
+      <Nav.Link href="/create">Create</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
+  <br/>
+  <Switch>
+<Route path='/' component={Content} exact/>
+<Route path='/Create' component={Header} exact/>
+<Route path='/read' component={Footer} exact/>
 
-      <Header></Header>
-        <Content></Content>
-        <Footer></Footer>
-    </div>
+  </Switch>
+
+     
+    </div></Router>
   );
 }
 }
